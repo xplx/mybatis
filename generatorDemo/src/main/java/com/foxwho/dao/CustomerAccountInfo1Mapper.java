e22ff09fce8335d02e9af6a8ea185e9bc463fc9e
wuxiaopeng <1099272020@qq.com>
2018-04-26 16:07:06 +0800

    表格生成

--- generatorDemo/src/main/java/com/foxwho/dao/CustomerAccountInfo1Mapper.java
+++
@@ -1,7 +0,0 @@
-package com.foxwho.dao;
-
-import com.foxwho.pojo.CustomerAccountInfo1;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface CustomerAccountInfo1Mapper extends Mapper<CustomerAccountInfo1> {
-}
